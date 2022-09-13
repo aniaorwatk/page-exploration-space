53b9acecc7c71bc46dae7ddc20ef1379e6b6241e
nazwaaniaorwatk <orwat.ania@gmail.com>
2022-09-13 21:14:24 +0200

    add title

--- src/component/Register/Register.tsx
+++ src/component/Register/Register.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import labels from "../../labels";
 import "./Register.scss"
 
 const Register =()=>{
     return(
         <div className="register">
 <div className="register__box">
-
+<h1>{labels.register.register}</h1>
 </div>
 
         </div>
