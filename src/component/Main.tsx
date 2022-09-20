269a999f5876893b5c8af7c2dceb60fb7853645c
nazwaaniaorwatk <orwat.ania@gmail.com>
2022-09-20 08:57:06 +0200

    header-center, border-top navigation

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header/Header";
+import Navigation from "./Navigation/Navigation";
 
 const Main = () => {
   return (
     <>
       <Header />
+      <Navigation />
     </>
   );
 };
 
-export default Header;
+export default Main;
---
+++ src/component/Navigation/Navigation.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Navigation.scss';
+
+const Navigation =()=>{
+    return(
+       <div className="navigation">
+<div className="navigation__box">
+    <nav>
+        
+    </nav>
+</div>
+
+       </div> 
+    )
+}
+
+export default Navigation
