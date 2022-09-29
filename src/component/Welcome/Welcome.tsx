ebc56b33386e2aa9fe84ed6c139aac4f0628ea25
nazwaaniaorwatk <orwat.ania@gmail.com>
2022-09-29 12:44:48 +0200

    add welcome text to labels

--- src/component/Welcome/Welcome.tsx
+++ src/component/Welcome/Welcome.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import labels from "../../labels";
 import "./Welcome.scss";
 
 const Welcome = () => {
@@ -7,16 +8,16 @@ const Welcome = () => {
   let timeOfDay;
 
   if (hours < 12) {
-    timeOfDay = "morning";
+    timeOfDay = labels.welcome.morning;
   } else if (hours >= 12 && hours < 17) {
-    timeOfDay = "afternoon";
-  } else if ((hours >= 17 && hours < 21)){
-    timeOfDay = "evening";
-  }else {
-    timeOfDay = "night";
+    timeOfDay = labels.welcome.afternoon;
+  } else if (hours >= 17 && hours < 21) {
+    timeOfDay = labels.welcome.evening;
+  } else {
+    timeOfDay = labels.welcome.night;
   }
 
-  return <p className="welcomeExplorer">Good {timeOfDay} explorer</p>;
+  return <p className="welcomeExplorer">{labels.welcome.textWelcomeBeginning} {timeOfDay} {labels.welcome.textWelcomeEnd}</p>;
 };
 
 export default Welcome;
