6e580105bababed94129b9a5fe54130e0c016d36
nazwaaniaorwatk <orwat.ania@gmail.com>
2022-11-21 13:24:08 +0100

    add style article

--- src/component/Information/Article/Article.tsx
+++ src/component/Information/Article/Article.tsx
@@ -1,15 +1,13 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import data from "./../../../data/information";
 import "./Article.scss";
 
 interface ITypArticle {
   id: number;
   on: boolean;
   title: string;
-  firstPartArticle: string;
-  fullAricle: string;
+  firstPartArticle: string | JSX.Element;
+  fullAricle: string | JSX.Element;
   toggle: (id: number) => void;
+  children?: React.ReactNode;
 }
 
 const Article = ({
@@ -20,17 +18,30 @@ const Article = ({
   fullAricle,
   toggle,
 }: ITypArticle) => {
+
+  // const learn = {
+
+  //           <span className="article__text-learn" onClick={() => toggle(id)}>
+  //         {on ? "Learn less" : "Learn more..."}
+  //       </span>  
+
+  // }
   return (
-    <div>
-      <h3>{title}</h3>
-      {on ? (
-        <article>{fullAricle} </article>
-      ) : (
-        <article> {firstPartArticle}  </article>
-      )}
-      <span onClick={() => toggle(id)}>
-        {on ?   "Learn less" :"Learn more..."}
-      </span>
+    <div className="article">
+      <h3 className="article__title">{title}</h3>
+      <div className="article__text" >
+        {on ? (
+          <article className="article__text-full">{fullAricle}<span className="article__text-learn" onClick={() => toggle(id)}>
+          {on ? "Learn less" : "Learn more..."}
+        </span>  </article>
+        ) : (
+          <article className="article__text-short">
+            {firstPartArticle}             <span className="article__text-learn" onClick={() => toggle(id)}>
+          {on ? "Learn less" : "Learn more..."}
+        </span>  
+          </article>
+        )}
+      </div>
     </div>
   );
 };
--- src/component/Information/Information.tsx
+++ src/component/Information/Information.tsx
@@ -1,43 +1,37 @@
 import React from "react";
 import Article from "./Article/Article";
-import data from "./../../data/information"
-import "./Information.scss"
-import { assertQualifiedTypeIdentifier } from "@babel/types";
+import data from "./../../data/information";
+import "./Information.scss";
 
-const Information =()=>{
+const Information = () => {
+  const [articles, setArticles] = React.useState(data);
 
-    const [articles, setArticles] = React.useState(data)
+  function toggle(id: number) {
+    setArticles((prevArticles) => {
+      return prevArticles.map((article) => {
+        return article.id === id ? { ...article, on: !article.on } : article;
+      });
+    });
+  }
+
+  const squareElements = articles.map((article) => (
+    <Article
+      key={article.id}
+      id={article.id}
+      on={article.on}
+      toggle={toggle}
+      title={article.title}
+      firstPartArticle={article.firstPartArticle}
+      fullAricle={article.fullAricle}
+    />
+  ));
+
+  return <div className="informationBox">
+    <h2>Information</h2>
     
-    function toggle(id: number) {
-        setArticles(prevArticles => {
-            return prevArticles.map((article) => {
-                return article.id === id ? {...article, on: !article.on} : article
-            })
-        })
-    }
+    {squareElements}
     
-    const squareElements = articles.map(article => (
-        <Article 
-            key={article.id}
-            id={article.id}
-            on={article.on}
-            toggle={toggle} 
-            title={article.title} 
-            firstPartArticle={article.firstPartArticle} 
-            fullAricle={article.fullAricle}        />
-    ))
+    </div>;
+};
 
-
-
-
-
-    return(
-        <div>
-           {squareElements}
-        </div>
-    )
-}
-
-export default Information
-
-
+export default Information;
